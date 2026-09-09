9e0de1f162dc6956aaa542daa2c1843e124a8604
Ifihanagbara Olusheye <victoriaolusheye@gmail.com>
2026-09-09 17:34:51 +0200

    chore: add Flutter project scaffold (Android, iOS, web platform folders)

---
+++ app/README.md
@@ -0,0 +1,5 @@
+# 60 Seconds — App
+
+Flutter mobile app for iOS and Android.
+
+> Coming soon. This directory will contain the Flutter project.
---
+++ app/android/app/src/main/kotlin/dev/sixtyseconds/sixty_seconds/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.sixtyseconds.sixty_seconds
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
